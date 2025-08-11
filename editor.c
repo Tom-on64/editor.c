8b764cd8de7bea6b3891a9c5a18cfb524e32f0dd
Tomas <kubecekt@gmail.com>
2025-08-11 11:03:40 +0200

    Got basic input handling & setup

--- editor.c
+++ editor.c
@@ -22,10 +22,13 @@ static struct termios orig_termios;
 /*
  * Generic helpers
  */
+#define CTRL_KEY(_k) ((_k) & 0x1F)
+
 noreturn
 static void die(const char* s) {
 	fprintf(
 		stderr,
+		"\x1b[2J\x1b[H"
 		"%s: %s\n",
 		s, strerror(errno)
 	       );
@@ -55,6 +58,34 @@ static void enable_raw(void) {
 	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+static char read_key(void) {
+	int nread;
+	char c;
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+		if (nread == -1 && errno != EAGAIN) die("read");
+	}
+	return c;
+}
+
+/*
+ * Rendering
+ */
+static void refresh_screen(void) {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+/*
+ * Input handling
+ */
+static void process_keypress(void) {
+	char c = read_key();
+
+	switch (c) {
+	case CTRL_KEY('q'): exit(0); break;
+	}
+}
+
 /*
  * Initialization
  */
@@ -62,15 +93,8 @@ int main(void) {
 	enable_raw();
 
 	while (1) {
-		char c = '\0';
-		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-
-		if (c == 'q') break;
-
-		if (iscntrl(c)) printf("%d\n", c);
-		else printf("%d (%c)", c, c);
+		refresh_screen();
+		process_keypress();
 	}
-
-	exit(0);
 }
 
